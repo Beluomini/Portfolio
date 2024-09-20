8ffbffa51bd039ff6e8901bcaf35dfa4443bc193
Lucas Beluomini <lucasbeluomini1@gmail.com>
2024-09-20 19:48:20 +0000

    feat: changing the about view content to a component named AboutPage

---
+++ src/components/about/AboutPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="story">
+    <h1>Aoba! Bão?</h1>
+    <p>Aqui eu vou colocar minhas habilidades, stacks e curiosidades sobre mim</p>
+  </div>
+</template>
+
+<style>
+.story {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  border-radius: 8%;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,18 @@
+<script setup lang="ts">
+import AboutPage from '@/components/about/AboutPage.vue';
+</script>
+
 <template>
-  <div class="story">
-    <h1>Lucas Beluomini</h1>
-    <p>Aqui eu vou colocar minhas habilidades, stacks e curiosidades sobre mim</p>
-  </div>
+  <main>
+    <AboutPage />
+  </main>
 </template>
 
 <style>
 main {
   display: flex;
   flex-direction: column;
+  width: 80%;
   align-items: flex-start;
   justify-content: center;
 }
